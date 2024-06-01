bd9c7cfe93b5cc8dd8964988f5171d7afcd750ec
twnbay <twnbay@kookmin.ac.kr>
2024-06-01 15:10:53 +0900

    add random item

--- Item.cpp
+++ Item.cpp
@@ -29,12 +29,17 @@ void Item::generateItem(GameMap& map, std::vector<Item>& items) {
 
         // 해당 위치가 빈 공간인 경우, 그 위치에 아이템 추가
         if (map.getMap(x, y) == 0) {
-            // GROWTH = 0, POISON = 1
-            int type = rand() % 2;
+            // GROWTH = 0, POISON = 1, RANDOM = 2
+            int type = rand() % 3;
             // 벡터에 새로 만들 아이템 추가
             items.emplace_back(x, y, type); 
             // printMap 작동을 위해 GROWTH일 경우 5로, POISON일 경우 6으로 인자를 넘김
-            map.setMap(x, y, type == GROWTH ? 5 : 6); 
+            switch (type)
+            {
+                case GROWTH: map.setMap(x, y, 5); break;
+                case POISON: map.setMap(x, y, 6); break;
+                case RANDOM: map.setMap(x, y, 8); break;
+            }
         }
     }
 }
@@ -59,10 +64,16 @@ void Item::itemEffect(GameMap& map, Snake& snake, std::vector<Item>& items) {
         
         // 둘의 위치가 동일할 경우 아이템 효과 적용
         if (it->getX() == headX && it->getY() == headY) {  
-            if (it->getType() == GROWTH) {
-                snake.grow();
-            } else if (it->getType() == POISON) {
-                snake.shrink();
+            switch (it->getType()) {
+                case GROWTH: snake.grow(); break;
+                case POISON: snake.shrink(); break;
+                case RANDOM: // 랜덤으로 길이가 증가하거나 감소
+                    if (rand() % 2 == 0) {
+                        snake.grow();
+                    } else {
+                        snake.shrink();
+                    }
+                    break;
             }
             // 아이템을 맵에서 제거
             map.setMap(it->getX(), it->getY(), 0);
